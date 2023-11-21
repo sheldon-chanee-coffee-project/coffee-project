638f2f95ed9b1d2cd5a5ca124797e5652cf11bea
Sheldon Pasciak <sheldon@pasciak.com>
2023-11-21 12:46:02 -0700

    Restructure order of code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,44 @@
 
 (() => {
 
+    // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+    let coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
+
+    const coffeeContainerElement = document.querySelector('#coffee-container');
+
+    const submitButton = document.querySelector('#submit');
+    submitButton.addEventListener('click', updateCoffees);
+
+    const btnClear = document.querySelector('#btnClear');
+    btnClear.addEventListener('click', clearAddForm);
+
+    const roastSelection = document.querySelector('#roast-selection');
+    roastSelection.addEventListener('change', updateCoffees);
+
+    const coffeeName = document.querySelector('#coffeeName');
+    coffeeName.addEventListener('input', updateCoffees);
+
+    const addForm = document.querySelector("#submitForm");
+    addForm.addEventListener('submit', addCoffee);
+
+    const addRoastSelection = document.querySelector('#add-roast-selection');
+    const addCoffeeName = document.querySelector('#name');
+
     function renderCoffee(coffee) {
         return `
     <div class="coffee ${coffee.roast}">
@@ -40,41 +78,6 @@
 
     }
 
-    // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-    let coffees = [
-        {id: 1, name: 'Light City', roast: 'light'},
-        {id: 2, name: 'Half City', roast: 'light'},
-        {id: 3, name: 'Cinnamon', roast: 'light'},
-        {id: 4, name: 'City', roast: 'medium'},
-        {id: 5, name: 'American', roast: 'medium'},
-        {id: 6, name: 'Breakfast', roast: 'medium'},
-        {id: 7, name: 'High', roast: 'dark'},
-        {id: 8, name: 'Continental', roast: 'dark'},
-        {id: 9, name: 'New Orleans', roast: 'dark'},
-        {id: 10, name: 'European', roast: 'dark'},
-        {id: 11, name: 'Espresso', roast: 'dark'},
-        {id: 12, name: 'Viennese', roast: 'dark'},
-        {id: 13, name: 'Italian', roast: 'dark'},
-        {id: 14, name: 'French', roast: 'dark'},
-    ];
-
-    // When the page loads, the coffees should be sorted by their ids in ascending order.
-    coffees.sort((a, b) => a.id - b.id);
-
-    const coffeeContainerElement = document.querySelector('#coffee-container');
-
-    const submitButton = document.querySelector('#submit');
-    submitButton.addEventListener('click', updateCoffees);
-
-    const roastSelection = document.querySelector('#roast-selection');
-    roastSelection.addEventListener('change', updateCoffees);
-
-    const coffeeName = document.querySelector('#coffeeName');
-    coffeeName.addEventListener('input', updateCoffees);
-
-    const addRoastSelection = document.querySelector('#add-roast-selection');
-    const addCoffeeName = document.querySelector('#name');
-
     function getHighestId(array) {
         return array.reduce((prev, curr) => {
             return prev > curr.id ? prev : curr.id;
@@ -104,17 +107,11 @@
         updateCoffees();
     }
 
-    const addForm = document.querySelector("#submitForm");
-    addForm.addEventListener('submit', addCoffee);
-
     function clearAddForm() {
         roastSelection.value = 'all';
         coffeeName.value = '';
         updateCoffees();
     }
-
-    const btnClear = document.querySelector('#btnClear');
-    btnClear.addEventListener('click', clearAddForm);
 
     // (Note that any new coffees you add will be lost when you refresh the page, for an extra challenge,
     // research how localStorage works and see if you can find a way to persist the data).
@@ -126,11 +123,19 @@
         return JSON.parse(window.localStorage.getItem(key));
     }
 
-    let loadedCoffees = getFromLocalStorage('coffeeArrayData');
-    if (loadedCoffees) {
-        coffees = loadedCoffees;
+    function initialize() {
+
+        let loadedCoffees = getFromLocalStorage('coffeeArrayData');
+
+        if (loadedCoffees) {
+            coffees = loadedCoffees;
+        }
+
+        coffees.sort((a, b) => a.id - b.id);
+
+        coffeeContainerElement.innerHTML = renderCoffees(coffees);
     }
 
-    coffeeContainerElement.innerHTML = renderCoffees(coffees);
+    initialize();
 
 })(); // IIFE
